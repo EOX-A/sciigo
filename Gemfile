b15daf1a50535892b27a2e00f99796faf1d552e6
Marko Locher <marko@locher.at>
2013-01-08 18:38:38 +0100

    message building based on the liquid template engine, refactored sending, added message templates

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'https://rubygems.org'
 
+# liquid template engine
+gem 'liquid'
+
 group :test do
   gem 'spinach'
   # gem 'rspec'
--- lib/message.rb
+++ lib/message.rb
@@ -1,28 +1,63 @@
+require 'uri'
+
 module Sciigo
   class Message
-    def initialize
-      @notification = Sciigo::Nagios.new
+    @@templates = ::YAML.load_file('config/templates.yml')
+
+    def initialize(notification)
+      @notification = notification
       @time = Time.now
+      @template = @@templates[ @notification.category ][ @notification.type ]
     end
 
-    def to
-      recipient ||= @notification.contactemail
+    def recipient
+      @recipient ||= @notification.contactemail
     end
 
-    def content
-      ""
+    def message
+      return @message ||= Liquid::Template.parse(template('message'))
     end
 
     def title
-      ""
+      return @message ||= Liquid::Template.parse(template('title')) 
     end
 
     def time
       @time
     end
 
-    def url
+    def priority
+      @priority ||= template('priority') == 1
+    end
 
+    def url
+      # service notifications
+      if @notification.service?
+        return @notification['serviceactionurl'] if @notification.has_key?('serviceactionurl')
+        return "#{Sciigo.config['nagios']['extinfo']}?type=2&host=#{@notification['hostname']}&service=#{URI.escape(@notification['servicedesc'])}"
+      end
+
+      # host notifications
+      if @notification.host?
+        return @notification['hostactionurl'] if @notification.has_key?('hostactionurl')
+        return "#{Sciigo.config['nagios']['extinfo']}?type=1&host=#{@notification['hostname']}"
+      end
+
+      # link to the main nagios site if all else fails
+      return Sciigo.config['nagios']['url'] if Sciigo.config['nagios'].has_key?('url')
+    end
+
+    def notification
+      @notification
+    end
+
+    private
+    def template(tmpl)
+      raise Sciigo::Error if tmpl.nil? || tmpl.empty?
+      return @@templates[ @notification.category ][ @notification.type ][ tmpl ] if @@templates[ @notification.category ][ @notification.type ].has_key? tmpl
+      return @@templates[ @notification.category ][ 'default' ][ tmpl ] if @@templates[ @notification.category ][ 'default' ].has_key? tmpl
+      return @@templates[ 'default' ][ tmpl ] if @@templates[ 'default' ].has_key? tmpl
+      return nil
     end
 
   end
